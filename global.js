1cbc708afeabac7e7a5703211964df64f38e339f
KnowScratcher <yianlee2008@gmail.com>
2024-06-06 11:07:46 +0800

    更新版本

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 function load_info() {
     console.log("ok")
-    document.getElementById("version").innerHTML = "v2.0.0";
+    document.getElementById("version").innerHTML = "v2.0.1";
     document.getElementById("cc").innerHTML = "Copyright © Know Scratcher 2023<br>CC BY-NC-SA"
 }
 
